0ff5c095f7c9dd251a7afa92f2a935129e172254
Tasnim Abuayyash <tasnimabuayyash@gmail.com>
2026-02-27 16:23:58 +0300

    fixed concatenation of path in get

--- routing.cpp
+++ routing.cpp
@@ -6,7 +6,7 @@
 /*   By: tabuayya <tabuayya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 11:17:30 by balhamad          #+#    #+#             */
-/*   Updated: 2026/02/27 15:19:06 by tabuayya         ###   ########.fr       */
+/*   Updated: 2026/02/27 16:23:23 by tabuayya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,16 @@ int routNOW(client &cli, server &srv, const LocationConfig& locConfig)
 	return 0;
 }
 
-int get_method(client &cli, server &srv, const LocationConfig& locConfig)
+int get_method(client &cli, server &srv, const LocationConfig& locConfig, std::string uri)
 {
-	std::string str = locConfig.getRoot() + cli.getReq().getUri();
-	const char *chr_str = str.c_str();
+	//I also have uri
+	std::string Path = locConfig.getPath(); //location path
+	if(uri.find(Path) == 0) //remove location path from uri
+		uri.erase(0, Path.length());
+	std::string root = locConfig.getRoot(); //root
+	if(root[root.length() - 1] != '/' && uri[0] == '/')
+		root.insert(0, "/");
+	// srv.setRoot(root);
 	cli.setFileFd(open(chr_str, O_RDONLY));
 	//if directory => handle index/autoindex (not implemented yet, but don’t treat as file
 	if(cli.getFileFd() < 0)
@@ -61,6 +67,69 @@ int post_method(client &cli, server &srv, const LocationConfig& locConfig)
 	file << cli.getReq().getBody();
 	return 0;
 }
+int handleRouting(client &cli, server &srv)
+{
+	std::string uri = cli.getReq().getUri();
+	// TODO later: Check Host header here to see if we need to swap 'srv' for a virtual server
+	const std::map<std::string, LocationConfig>& locations = srv.getLocations();
+	const LocationConfig* loc = findLongestMatch(uri, locations);
+	if (loc != NULL)
+	{
+		if(routNOW(cli, srv, *loc) == 1)
+		{
+			cli.getRes().setStatusCode(METHOD_NOT_ALLOWED);
+			cli.setState("SENDING RESPONSE");
+			return -1; //405
+		}
+		if(cli.getContentLength() > loc->getMaxBodySize())
+		{
+			cli.getRes().setStatusCode(PAYLOAD_TOO_LARGE);
+			return -1;//403
+		}
+		if (cli.getReq().getMethod() == "GET")
+		{
+			get_method(cli, srv, *loc, uri);
+			cli.setState("SENDING RESPONSE"); // Ready to send the file back!
+		}
+		else if (cli.getReq().getMethod() == "POST")
+		{
+			post_method(cli, srv, *loc);
+			cli.setState("SENDING RESPONSE");
+		}
+		else if (cli.getReq().getMethod() == "DELETE")
+		{
+			//delete_method(cli, srv, *loc);
+			cli.setState("SENDING RESPONSE");
+		}
+	}
+	else
+	{
+		//go to srv defaults
+	}
+}
+const LocationConfig* findLongestMatch(const std::string& uri, const std::map<std::string, LocationConfig>& locations) //or locationobj
+{
+	const LocationConfig* longestmatch = NULL;
+	int max_size  = 0;
+	for (std::map<std::string, LocationConfig>::const_iterator it = locations.begin(); it != locations.end(); ++it)
+	{
+		const std::string& locationPath = it->first;
+		if (uri.compare(0, locationPath.size(), locationPath) == 0)
+		{
+			if (locationPath[locationPath.length() - 1] == '/' || uri.size() == locationPath.size() || uri[locationPath.size()] == '/')
+			{
+				if (locationPath.size() > max_size)
+				{
+					max_size = locationPath.size();
+					longestmatch = &(it->second);
+				}
+			}
+		}
+	}
+	return longestmatch;
+}
+
+
 // int handleRouting(client &cli, server &srv)
 // {
 // 	std::string uri = cli.getReq().getUri();
@@ -104,43 +173,3 @@ int post_method(client &cli, server &srv, const LocationConfig& locConfig)
 // 	}
 // 	return 0;
 // }
-int handleRouting(client &cli, server &srv)
-{
-	std::string uri = cli.getReq().getUri();
-	const std::map<std::string, LocationConfig>& locations = srv.getLocations();
-	const LocationConfig* loc = findLongestMatch(uri, locations);
-	if (loc != NULL)
-	{
-		if(cli.getContentLength() > loc->getMaxBodySize())
-		{
-			cli.getRes().setStatusCode(PAYLOAD_TOO_LARGE);
-			return -1;//403
-		}
-	}
-	else
-	{
-
-	}
-}
-const LocationConfig* findLongestMatch(const std::string& uri, const std::map<std::string, LocationConfig>& locations) //or locationobj
-{
-	const LocationConfig* longestmatch = NULL;
-	int max_size  = 0;
-	for (std::map<std::string, LocationConfig>::const_iterator it = locations.begin(); it != locations.end(); ++it)
-	{
-		const std::string& locationPath = it->first;
-		if (uri.compare(0, locationPath.size(), locationPath) == 0)
-		{
-			if (locationPath[locationPath.length() - 1] == '/' || uri.size() == locationPath.size() || uri[locationPath.size()] == '/')
-			{
-				if (locationPath.size() > max_size)
-				{
-					max_size = locationPath.size();
-					longestmatch = &(it->second);
-				}
-			}
-		}
-	}
-	return longestmatch;
-}
-
